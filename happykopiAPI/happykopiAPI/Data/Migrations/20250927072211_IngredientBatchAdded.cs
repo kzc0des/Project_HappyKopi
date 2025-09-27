3739511a601fa965253933b2ddbd29d7279302d2
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-09-27 16:43:01 +0800

    IngredientBatch Migrated

---
+++ happykopiAPI/happykopiAPI/Data/Migrations/20250927072211_IngredientBatchAdded.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace happykopiAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IngredientBatchAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Ingredients");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPerishable",
+                table: "Ingredients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "IngredientBatches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngredientId = table.Column<int>(type: "int", nullable: false),
+                    StockQuantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateReceived = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateUsed = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientBatches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientBatches_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientBatches_IngredientId",
+                table: "IngredientBatches",
+                column: "IngredientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientBatches");
+
+            migrationBuilder.DropColumn(
+                name: "IsPerishable",
+                table: "Ingredients");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StockQuantity",
+                table: "Ingredients",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
