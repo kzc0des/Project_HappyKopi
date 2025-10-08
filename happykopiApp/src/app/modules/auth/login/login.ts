af4ddf87493a4a9092c1726d9c2c5dcda2fe67eb
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-08 19:00:58 +0800

    Styles Applied

--- happykopiApp/src/app/modules/auth/login/login.ts
+++ happykopiApp/src/app/modules/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './login.css'
 })
 export class Login {
+  passwordVisible = signal(false);
 
+  passwordFieldType = computed(() => this.passwordVisible() ? 'text' : 'password');
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible.update(visible => !visible);
+  }
 }
