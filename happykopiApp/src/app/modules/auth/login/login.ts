590d62e6749068ea23f2aa29c723a353bb32dbc6
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-08 19:52:33 +0800

    Static Login Page

--- happykopiApp/src/app/modules/auth/login/login.ts
+++ happykopiApp/src/app/modules/auth/login/login.ts
@@ -1,8 +1,9 @@
 import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
@@ -13,5 +14,11 @@ export class Login {
 
   togglePasswordVisibility(): void {
     this.passwordVisible.update(visible => !visible);
+  }
+
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log('Form Submitted!', form.value);
+    }
   }
 }
