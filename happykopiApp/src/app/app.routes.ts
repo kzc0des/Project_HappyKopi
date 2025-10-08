7df03564534d0d66496a9f4ea66f21a495269bc3
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-08 18:26:04 +0800

    Router Setup

--- happykopiApp/src/app/app.routes.ts
+++ happykopiApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './modules/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'Login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'Login',
+        component: Login
+    }
+];
