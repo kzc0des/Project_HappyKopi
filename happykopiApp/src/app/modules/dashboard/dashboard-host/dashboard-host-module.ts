ce6062e1d86998f22f9f7f50b2e777ed99fd4076
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-12 11:15:21 +0800

    Added Dashboard-Host Component

--- happykopiApp/src/app/modules/dashboard/dashboard-host/dashboard-host-module.ts
+++ happykopiApp/src/app/modules/dashboard/dashboard-host/dashboard-host-module.ts
@@ -11,11 +11,19 @@ import { AuthService } from '../../../core/services/auth/auth.service';
     CommonModule
   ]
 })
-export class DashboardHostModule implements OnInit{ 
+export class DashboardHostModule implements OnInit {
 
-  constructor (private router:Router, private authService:AuthService) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
-    
+    const userRole = this.authService.getUserRole();
+
+    if (userRole === 'Admin') {
+      this.router.navigate(['/app/dashboard/admin'], { replaceUrl: true });
+    } else if (userRole === 'Barista') {
+      this.router.navigate(['/app/dashboard/barista'], { replaceUrl: true });
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
