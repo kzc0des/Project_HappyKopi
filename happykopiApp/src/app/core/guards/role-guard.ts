a57d6448e339781e702497751ff7ecc4d430e7fd
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-12 11:12:52 +0800

    Refactored Role Guard

--- happykopiApp/src/app/core/guards/role-guard.ts
+++ happykopiApp/src/app/core/guards/role-guard.ts
@@ -1,22 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
-import { map, take } from 'rxjs';
 
 export const roleGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   const requiredRoles = route.data['roles'] as Array<string>;
-  return authService.getCurrentUser$().pipe(
-    take(1), 
-    map(user => {      
-      if (user && requiredRoles.includes(user.role)) {
-        return true;
-      } else {
-        console.error('Unauthorized access: Required roles', requiredRoles, ', User role:', user?.role);
-        return router.createUrlTree(['/login']);
-      }
-    })
-  );
+  const userRole = authService.getUserRole();
+
+  if (userRole && requiredRoles.includes(userRole)) {
+    return true;
+  }
+
+  console.error('Unauthorized access attempt!');
+  return router.createUrlTree(['/pos']);
 };
