854d5b0133c3b1bb6e50d11388247fd26626a27c
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-12 17:44:00 +0800

    Sidebar Added

---
+++ happykopiApp/src/app/core/models/navitem.ts
@@ -0,0 +1,5 @@
+export interface Navitem {
+    id: string,
+    icon: string,
+    label: string
+}
--- happykopiApp/src/app/layouts/main-layout/main-layout.ts
+++ happykopiApp/src/app/layouts/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "../../shared/components/header/header";
+import { Sidebar } from "../../shared/components/sidebar/sidebar";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
