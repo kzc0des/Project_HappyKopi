b7015e7f20b3a20cf1a79d6ce6a1bf90c3d144ef
Lawrence <Lawrence@ACERRYZEN>
2025-10-13 18:40:17 +0800

    Added routs

---
+++ happykopiApp/src/app/shared/components/orders/order-item-card/order-item-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-item-card',
+  imports: [],
+  templateUrl: './order-item-card.html',
+  styleUrl: './order-item-card.css'
+})
+export class OrderItemCard {
+
+}
