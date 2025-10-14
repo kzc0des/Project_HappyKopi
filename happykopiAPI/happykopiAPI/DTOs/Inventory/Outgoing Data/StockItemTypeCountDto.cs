7f1cbfd85a462c671ef4b0eb20aa2d0b0d46cae3
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-14 23:56:46 +0800

    Added StockItemTypeCountDto

---
+++ happykopiAPI/happykopiAPI/DTOs/Inventory/Outgoing Data/StockItemTypeCountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace happykopiAPI.DTOs.Inventory.Outgoing_Data
+{
+    public class StockItemTypeCountDto
+    {
+        public string ItemTypeName { get; set; }
+        public int StockItemCount { get; set; }
+    }
+}
