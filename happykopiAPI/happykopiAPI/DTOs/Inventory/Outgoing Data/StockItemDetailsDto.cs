187f5f699d11f0d25676d3b7f928f87c6d78d746
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-15 00:50:51 +0800

    Refactored StockItemDetailsDto

--- happykopiAPI/happykopiAPI/DTOs/Inventory/Outgoing Data/StockItemDetailsDto.cs
+++ happykopiAPI/happykopiAPI/DTOs/Inventory/Outgoing Data/StockItemDetailsDto.cs
@@ -11,6 +11,7 @@
         public int ItemType { get; set; }
         public bool IsActive { get; set; }
         public decimal TotalStockQuantity { get; set; }
-        public int BatchCount { get; set; }
+
+        public List<StockItemBatchDetailsDto> Batches { get; set; }
     }
 }
