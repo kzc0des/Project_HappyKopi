7fb273d3259794e4b56231819b0499296e922ace
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-14 23:21:07 +0800

    Added File Placeholder For POS Components

---
+++ happykopiApp/src/app/modules/pos/components/tobedel-card/tobedel-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tobedel-card',
+  imports: [],
+  templateUrl: './tobedel-card.html',
+  styleUrl: './tobedel-card.css'
+})
+export class TobedelCard {
+
+}
