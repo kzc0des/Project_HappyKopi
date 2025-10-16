285d5ce3f2aa93fb6797639f4ec0d4be3425b823
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-17 01:07:19 +0800

    Refactored itemcard component

--- happykopiApp/src/app/shared/components/itemcard/itemcard.ts
+++ happykopiApp/src/app/shared/components/itemcard/itemcard.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-itemcard',
@@ -7,6 +7,13 @@ import { Component, Input } from '@angular/core';
   styleUrl: './itemcard.css'
 })
 export class Itemcard {
-  @Input() itemTitle: string = '';
-  @Input() itemName: string = '';
+  @Input() itemTitle: string = 'Item Title';
+  @Input() value: string | number = '';
+  @Input() isEditing: boolean = false;
+  @Output() valueChange = new EventEmitter<string>();
+
+  onValueChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.valueChange.emit(target.value);
+  }
 }
