879e9af088abe4f58cdd9c99a9e5ae5b87822941
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-18 18:43:36 +0800

    Added delete method and adjusted edit method

--- happykopiApp/src/app/modules/inventory/inventory-item-detail/inventory-item-detail.ts
+++ happykopiApp/src/app/modules/inventory/inventory-item-detail/inventory-item-detail.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StockItemDetailsDto } from '../../../core/dtos/stockitem/stock-item-details-dto';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { IngredientBatchCard } from "../components/ingredient-batch-card/ingredient-batch-card";
 import { Itemcard } from "../../../shared/components/itemcard/itemcard";
 import { Subscription } from 'rxjs';
@@ -12,7 +12,7 @@ import { DropdownOption } from '../../../shared/components/dropdown-button/dropd
 import { DropdownButton } from "../../../shared/components/dropdown-button/dropdown-button";
 import { Stockitemtype } from '../../../core/enums/stockitemtype';
 import { StockItemForUpdateDto } from '../../../core/dtos/stockitem/stock-item-for-update-dto';
-import { JsonPipe } from '@angular/common';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -38,6 +38,7 @@ export class InventoryItemDetail implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private headerActionService: HeaderService,
     private inventoryService: InventoryService,
+    private location: Location,
   ) { }
 
   ngOnInit(): void {
@@ -63,7 +64,7 @@ export class InventoryItemDetail implements OnInit, OnDestroy {
           break;
         case 'DELETE':
           if (confirm('Are you sure you want to delete this item?')) {
-
+            this.deleteStockItem();
           }
           break;
         case 'CANCEL':
@@ -115,6 +116,14 @@ export class InventoryItemDetail implements OnInit, OnDestroy {
   }
 
   private deleteStockItem(): void {
-      
+    this.inventoryService.deactivateStockItem(this.stockitemdetail.id).subscribe({
+      next: response => {
+        console.log(`Update successful. ${response}`);
+        this.location.back();
+      },
+      error: err => {
+        console.error(`Update failed ${err}`);
+      }
+    })
   }
 }
