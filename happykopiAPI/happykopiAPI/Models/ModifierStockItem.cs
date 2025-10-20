80b64c2a75195e89fced1cee03ed574707811a94
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-20 13:37:19 +0800

    Added ModifierStockItem Model and Table

---
+++ happykopiAPI/happykopiAPI/Models/ModifierStockItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace happykopiAPI.Models
+{
+    public class ModifierStockItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ModifierId { get; set; }
+
+        [ForeignKey("ModifierId")]
+        public Modifier Modifier { get; set; }
+
+        [Required]
+        public int StockItemId { get; set; }
+
+        [ForeignKey("StockItemId")]
+        public StockItem StockItem { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal QuantityNeeded { get; set; }
+    }
+}
