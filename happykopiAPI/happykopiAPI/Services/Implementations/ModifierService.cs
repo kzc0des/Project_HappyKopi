b6bed68d8961e62e139f7305e59a6cded894c95d
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-20 18:51:20 +0800

    Added INotificationService and SignalRNotificationService

--- happykopiAPI/happykopiAPI/Services/Implementations/ModifierService.cs
+++ happykopiAPI/happykopiAPI/Services/Implementations/ModifierService.cs
@@ -1,9 +1,44 @@
-﻿using happykopiAPI.Services.Interfaces;
+﻿using happykopiAPI.DTOs.Modifier.Incoming_Data;
+using happykopiAPI.DTOs.Modifier.Outgoing_Data;
+using happykopiAPI.Services.Interfaces;
 
 namespace happykopiAPI.Services.Implementations
 {
     public class ModifierService : IModifierService
     {
+        public ModifierService()
+        {
 
+        }
+
+        public Task<ModifierDto> CreateModifierAsync(ModifierForCreateDto modifierForCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ModifierDto>> GetAllModifiersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ModifierDto>> GetAvailableModifiersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LinkStockItemAsync(int modifierId, ModifierStockItemLinkDto linkDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UnlinkStockItemAsync(int modifierId, int stockItemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateModifierAsync(int modifierId, ModifierForUpdateDto modifierForUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ happykopiAPI/happykopiAPI/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿namespace happykopiAPI.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        /// <summary>
+        /// Notifies all connected clients that the list of modifiers has been updated.
+        /// </summary>
+        Task NotifyModifiersUpdatedAsync();
+    }
+}
