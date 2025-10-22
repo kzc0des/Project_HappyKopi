0af9b4f4f5be37db78ead45e36f0e66cdc96c9ba
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-22 16:56:36 +0800

    Refactored ModifiersTable and sp_AddNewModifiers

---
+++ happykopiAPI/happykopiAPI/Data/Migrations/20251022085500_Refactoredsp_AddNewModifier.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace happykopiAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refactoredsp_AddNewModifier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp_AddNewModifier = @"
+            CREATE OR ALTER PROCEDURE [dbo].[sp_AddNewModifier]
+                @Name NVARCHAR(100),
+                @Price DECIMAL(18, 2),
+                @Type NVARCHAR(50),
+                @OzAmount DECIMAL(18, 2) = NULL,
+                @IsAvailable BIT = 1
+            AS
+            BEGIN
+                SET XACT_ABORT ON;
+                SET NOCOUNT ON;
+                BEGIN TRANSACTION;
+                BEGIN TRY
+                    IF EXISTS (SELECT 1 FROM dbo.Modifiers WHERE Name = @Name)
+                        THROW 50005, 'A modifier with this name already exists.', 1;
+
+                    INSERT INTO dbo.Modifiers (Name, Price, Type, OzAmount, IsAvailable, LastUpdated) -- IDAGDAG ANG OzAmount
+                    VALUES (@Name, @Price, @Type, @OzAmount, @IsAvailable, GETDATE()); -- IDAGDAG ANG @OzAmount
+
+                    DECLARE @NewModifierId INT = SCOPE_IDENTITY();
+                    COMMIT TRANSACTION;
+                    SELECT @NewModifierId AS NewModifierId;
+                END TRY
+                BEGIN CATCH
+                    IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+                    THROW;
+                END CATCH
+            END";
+
+            migrationBuilder.Sql(sp_AddNewModifier);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS [dbo].[sp_AddNewModifier]");
+        }
+    }
+}
