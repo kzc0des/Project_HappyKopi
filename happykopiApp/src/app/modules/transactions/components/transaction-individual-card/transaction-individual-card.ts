71f3e92827222469520eaaf2661c7b3961e9d83d
neoaia <151616379+neoaia@users.noreply.github.com>
2025-10-27 16:40:55 +0800

    transaction individual card component

--- happykopiApp/src/app/modules/transactions/components/transaction-individual-card/transaction-individual-card.ts
+++ happykopiApp/src/app/modules/transactions/components/transaction-individual-card/transaction-individual-card.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface TransactionIndividualCardDto {
+  TotalAmount: number,
+  TotalTransactions: number;
+}
 
 @Component({
   selector: 'app-transaction-individual-card',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './transaction-individual-card.html',
   styleUrl: './transaction-individual-card.css',
 })
-export class TransactionIndividualCard {
+export class TransactionIndividualCard implements OnInit {
+  @Input() cardMode: 'cash' | 'gcash' = 'gcash';
+  @Input() transactionCard?: TransactionIndividualCardDto;
 
-}
+  ngOnInit() {
+    // Default values: if parent doesn't pass transactionCard, create one
+    this.transactionCard = {
+      TotalAmount: this.transactionCard?.TotalAmount ?? 0,
+      TotalTransactions: this.transactionCard?.TotalTransactions ?? 0
+    };
+  }
+} 
