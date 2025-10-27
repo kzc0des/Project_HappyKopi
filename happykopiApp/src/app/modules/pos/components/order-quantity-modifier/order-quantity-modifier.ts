78000424db1debc3279a0b5ae5dd7fd93ccb8ae5
neoaia <151616379+neoaia@users.noreply.github.com>
2025-10-27 14:01:53 +0800

    add order modal component

--- happykopiApp/src/app/modules/pos/components/order-quantity-modifier/order-quantity-modifier.ts
+++ happykopiApp/src/app/modules/pos/components/order-quantity-modifier/order-quantity-modifier.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 export interface oqmDto {
   Quantity: number
@@ -10,6 +10,24 @@ export interface oqmDto {
   templateUrl: './order-quantity-modifier.html',
   styleUrl: './order-quantity-modifier.css'
 })
-export class OrderQuantityModifier {
+export class OrderQuantityModifier implements OnInit {
   @Input() oqm!: oqmDto;
+
+  ngOnInit() {
+    // ✅ Set default value to 1 if undefined or less than 1
+    if (!this.oqm || this.oqm.Quantity <= 0) {
+      this.oqm = { Quantity: 1 };
+    }
+  }
+
+  increase() {
+    this.oqm.Quantity++;
+  }
+
+  decrease() {
+    // ✅ Prevent decreasing to 0 or below
+    if (this.oqm.Quantity > 1) {
+      this.oqm.Quantity--;
+    }
+  }
 }
