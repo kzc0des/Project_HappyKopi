7efe7c053e8cc4041a789b1335f4ff24c95760ae
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-31 11:53:50 +0800

    Product routes initialized

--- happykopiApp/src/app/modules/products/components/category-button-field/category-button-field.ts
+++ happykopiApp/src/app/modules/products/components/category-button-field/category-button-field.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-button-field',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-button-field.html',
   styleUrl: './category-button-field.css'
 })
 export class CategoryButtonField {
     constructor(private router: Router) {}
-
-    goToCategory() {
-    this.router.navigate(['/categories-list-page'])
-  }
-
 }
 
