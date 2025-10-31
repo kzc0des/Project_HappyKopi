3fa4114bafd383f828afcdbbcb7ef437eec6a02e
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-31 12:15:51 +0800

    Adjusting category-list-card

--- happykopiApp/src/app/modules/products/components/category-list-card/category-list-card.ts
+++ happykopiApp/src/app/modules/products/components/category-list-card/category-list-card.ts
@@ -8,14 +8,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './category-list-card.css'
 })
 export class CategoryListCard {
-  @Input() items: any[] = [];
-  @Input() labelKey = 'name';       
-  @Input() subLabelKey?: string;   
-  @Input() subLabelSuffix?: string;   
-  @Input() clickable = true;          
-  @Output() itemClick = new EventEmitter<any>();
+  // @Input() items: any[] = [];
+  // @Input() labelKey = 'name';       
+  // @Input() subLabelKey?: string;   
+  // @Input() subLabelSuffix?: string;   
+  // @Input() clickable = true;          
 
-  onClick(item: any): void {
-    if (this.clickable) this.itemClick.emit(item);
-  }
+  isEditingProducts: boolean = false;
+  
+  @Input() itemTitle !: string;
+  @Input() itemValue !: number;
+  @Input() itemUnit !: string;
+  @Output() itemClick = new EventEmitter<any>();
 }
