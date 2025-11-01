4cf8124daa7b84801dc9bce1e20d242adffad522
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-01 10:43:32 +0800

    Refactored category service

--- happykopiApp/src/app/modules/categories/resolver/alldrinkscategorywithcount/all-drinks-category-with-count-resolver.ts
+++
@@ -1,9 +0,0 @@
-import { ResolveFn } from '@angular/router';
-import { CategoryWithProductCountDto } from '../../../../core/dtos/category/category-with-product-count-dto';
-import { Observable } from 'rxjs';
-import { inject } from '@angular/core';
-import { CategoryService } from '../../services/category.service';
-
-export const allDrinksCategoryWithCountResolver: ResolveFn<CategoryWithProductCountDto> = (route, state) : Observable<CategoryWithProductCountDto> => {
-  const categoryService = inject(CategoryService);
-};
