7eaf638864024aa9b677cc797dd2a76b0e239a2a
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-03 04:23:22 +0800

    Added image service via cloudinarydotnet

---
+++ happykopiAPI/happykopiAPI/Services/Interfaces/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace happykopiAPI.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<ImageUploadResult> AddImageAsync(IFormFile file);
+        Task<DeletionResult> DeleteImageAsync(string publicId);
+    }
+}
