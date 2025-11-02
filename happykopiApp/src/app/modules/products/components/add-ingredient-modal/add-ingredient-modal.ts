65d2829a714588ceff538bc095b9f9c3a97bded4
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-02 12:31:30 +0800

    Added styling and modal triggers for ingredient modal

--- happykopiApp/src/app/modules/products/components/add-ingredient-modal/add-ingredient-modal.ts
+++ happykopiApp/src/app/modules/products/components/add-ingredient-modal/add-ingredient-modal.ts
@@ -3,14 +3,29 @@ import { DropdownButton } from "../../../../shared/components/dropdown-button/dr
 import { Itemcard } from "../../../../shared/components/itemcard/itemcard";
 import { YellowButton } from "../../../../shared/components/yellow-button/yellow-button";
 import { RedButton } from "../../../../shared/components/red-button/red-button";
+import { Observable } from 'rxjs';
+import { ModalService } from '../../services/modal-service/modal.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-ingredient-modal',
-  imports: [DropdownButton, Itemcard, YellowButton, RedButton],
+  imports: [DropdownButton, Itemcard, YellowButton, RedButton, AsyncPipe],
   templateUrl: './add-ingredient-modal.html',
   styleUrl: './add-ingredient-modal.css',
 })
 export class AddIngredientModal {
   @Input() addons !: string;
   @Input() isEditing = false;
+
+  isIngredientOpen$: Observable<boolean>;
+
+  constructor (
+    private modalService: ModalService
+  ) {
+    this.isIngredientOpen$ = modalService.isIngredientModalOpen$;
+  }
+
+  close() {
+    this.modalService.closeIngredientModal();
+  }
 }
