0fb37c5c8ae1fea91d3ad44e720f1d0cef927a5b
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-02 19:48:37 +0800

    Added quantity-button component

--- happykopiApp/src/app/modules/products/components/add-addon-modal/add-addon-modal.ts
+++ happykopiApp/src/app/modules/products/components/add-addon-modal/add-addon-modal.ts
@@ -1,20 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DropdownButton } from "../../../../shared/components/dropdown-button/dropdown-button";
 import { YellowButton } from "../../../../shared/components/yellow-button/yellow-button";
 import { RedButton } from "../../../../shared/components/red-button/red-button";
 import { Observable } from 'rxjs';
 import { ModalService } from '../../services/modal-service/modal.service';
 import { AsyncPipe } from '@angular/common';
+import { DropdownOption } from '../../../../shared/components/dropdown-button/dropdown-option';
+import { AddOnItem } from '../../../../core/dtos/product/product.model';
+import { QuantityButton } from "../../../../shared/components/quantity-button/quantity-button";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-add-addon-modal',
-  imports: [DropdownButton, YellowButton, RedButton, AsyncPipe],
+  imports: [DropdownButton, YellowButton, RedButton, AsyncPipe, QuantityButton, FormsModule],
   templateUrl: './add-addon-modal.html',
   styleUrl: './add-addon-modal.css',
 })
-export class AddAddonModal{
+export class AddAddonModal {
   @Input() addons !: string;
   @Input() isEditing = false;
+
+  @Input() addOnOption: DropdownOption[] = [];
+  @Output() saveAddOn = new EventEmitter<AddOnItem>(); //payload
+
+  public selectedAddOnId: number | null = null;
+  public times: number = 1;
 
   isAddOnOpen$: Observable<boolean>;
 
@@ -26,5 +36,22 @@ export class AddAddonModal{
 
   close() {
     this.modalService.closeAddOnModal();
+    this.selectedAddOnId = null;
+    this.times = 1;
+  }
+
+  onSave() {
+    if (this.selectedAddOnId && this.times > 0) {
+
+      const payload: AddOnItem = {
+        addOnId: this.selectedAddOnId,
+        times: this.times
+      };
+
+      this.saveAddOn.emit(payload);
+      this.close();
+    } else {
+      console.log("Pumili muna ng add-on at maglagay ng valid na 'times'");
+    }
   }
 }
