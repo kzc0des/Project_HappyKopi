c4acc3d2cdd39689cc24b24776dd59638b7ef8b5
neoaia <151616379+neoaia@users.noreply.github.com>
2025-11-10 17:32:34 +0800

    view orders

--- happykopiApp/src/app/modules/pos/components/cart-item/cart-item.ts
+++ happykopiApp/src/app/modules/pos/components/cart-item/cart-item.ts
@@ -1,22 +1,23 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CartAddonDto } from '../../../../core/dtos/order/cart-addon.dto';
+import { CommonModule } from '@angular/common';
 
-export interface cartItemDto {
- Name: string,
- Size: string,
- DrinkImage: string,
- AddOn: string,
- Subtotal: number,
- DrinkQuantity: number,
- AddOnQuantity: number
-}
+  export interface cartItemDto {
+    Name: string;
+    Size: string;
+    DrinkImage: string;
+    Addons: CartAddonDto[];
+    Subtotal: number;
+    DrinkQuantity: number; 
+  }
 
 @Component({
   selector: 'app-cart-item',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, CommonModule],
   templateUrl: './cart-item.html',
-  styleUrl: './cart-item.css'
+  styleUrl: './cart-item.css',
 })
 export class CartItem {
-@Input() cartItem!: cartItemDto;
+  @Input() cartItem!: cartItemDto;
 }
--- happykopiApp/src/app/modules/pos/forms/view-order/view-order.ts
+++ happykopiApp/src/app/modules/pos/forms/view-order/view-order.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../../components/cart-item/cart-item';
-import { LongYellowButton } from "../../../../shared/components/long-yellow-button/long-yellow-button";
+import { LongYellowButton } from '../../../../shared/components/long-yellow-button/long-yellow-button';
+import { OrderItem } from '../../../../core/dtos/order/order-item.dto';
+import { cartItemDto } from '../../components/cart-item/cart-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-order',
-  imports: [CartItem, LongYellowButton],
+  imports: [CartItem, LongYellowButton, CommonModule],
   templateUrl: './view-order.html',
-  styleUrl: './view-order.css'
+  styleUrl: './view-order.css',
 })
-export class ViewOrder {
+export class ViewOrder implements OnInit {
+  orders: cartItemDto[] = [];
 
+  ngOnInit() {
+    this.loadOrders();
+  }
+
+  loadOrders() {
+    const storedOrders: OrderItem[] = JSON.parse(localStorage.getItem('orders') || '[]');
+
+    this.orders = storedOrders.map((order) => ({
+      Name: order.drinkName,
+      Size: order.size,
+      DrinkImage: '',
+      Addons: order.addons.map((a) => ({ name: a.name, quantity: a.quantity })),
+      Subtotal: order.total,
+      DrinkQuantity: order.quantity,
+    }));
+  }
+
+  clearOrders() {
+    localStorage.removeItem('orders'); 
+    this.orders = [];                  
+  }
 }
