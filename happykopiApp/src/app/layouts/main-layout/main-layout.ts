d52720a5cd6f61325c72f145e2e0b73d8b0c5076
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-11 17:53:20 +0800

    Alert modal component added

--- happykopiApp/src/app/layouts/main-layout/main-layout.ts
+++ happykopiApp/src/app/layouts/main-layout/main-layout.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "../../shared/components/header/header";
 import { Sidebar } from "../../shared/components/sidebar/sidebar";
 import { ConfirmationDialog } from "../../shared/components/confirmation-dialog/confirmation-dialog";
+import { AlertDialog } from '../../shared/components/alert-dialog/alert-dialog';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Header, Sidebar, ConfirmationDialog],
+  imports: [RouterOutlet, Header, Sidebar, ConfirmationDialog, AlertDialog],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
