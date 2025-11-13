5f9e8a0925b619a4f132b8571bfef60cdba865b5
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-13 14:15:57 +0800

    Designed batch-add-page

--- happykopiApp/src/app/modules/inventory/inventory-batch-add/inventory-batch-add.ts
+++ happykopiApp/src/app/modules/inventory/inventory-batch-add/inventory-batch-add.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Itemcard } from '../../../shared/components/itemcard/itemcard';
+import { DateCard } from '../../../shared/components/date-card/date-card';
+import { ExpiryDateCard } from '../components/expiry-date-card/expiry-date-card';
+import { FormsModule } from '@angular/forms';
+import { StockItemBatchForCreateDto } from '../../../core/dtos/stockitem/stock-item-batch-for-create-dto';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-inventory-batch-add',
-  imports: [],
+  imports: [Itemcard, DateCard, ExpiryDateCard, FormsModule],
   templateUrl: './inventory-batch-add.html',
   styleUrl: './inventory-batch-add.css',
 })
-export class InventoryBatchAdd {
+export class InventoryBatchAdd implements OnInit {
+  batchDetail: Partial<StockItemBatchForCreateDto> = {};
+  dateAdded: Date = new Date();
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
 }
