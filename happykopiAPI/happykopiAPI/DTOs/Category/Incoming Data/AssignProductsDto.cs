cac24297c3be8e7299f9ccbaed3d98951555992a
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-14 06:50:14 +0800

    Added assign-product-category dto

---
+++ happykopiAPI/happykopiAPI/DTOs/Category/Incoming Data/AssignProductsDto.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace happykopiAPI.DTOs.Category.Incoming_Data
+{
+    public class AssignProductsDto
+    {
+        public List<int> ProductIds { get; set; }
+    }
+}
---
+++ happykopiApp/src/app/core/dtos/category/assign-products-to-category-dto.ts
@@ -0,0 +1,3 @@
+export interface AssignProductsToCategoryDto {
+    productIds: number[];
+}
