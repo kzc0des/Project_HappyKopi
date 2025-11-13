15dca8ebda7fda41c311f0bcc546603ff2661fe2
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-14 04:28:21 +0800

    Test changes for routing - refactored data rendering in modifier-edit-page

--- happykopiApp/src/app/core/dtos/modifier/modifier-link-stock-item.model.ts
+++ happykopiApp/src/app/core/dtos/modifier/modifier-link-stock-item.model.ts
@@ -1,5 +1,5 @@
 export interface ModifierLinkStockItem {
     stockItemId: number;
-    stockItemName: string;
-    quantityNeeded: number;
+    stockItemName?: string;
+    quantityNeeded?: number;
 }
