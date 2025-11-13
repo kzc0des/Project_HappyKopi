6aa1de0461e48fbdb227d2983d91e251d625dc43
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-14 01:35:11 +0800

    Test changes for product-categories page - added passing of the categoryId via queryParams

--- happykopiApp/src/app/modules/products/product-pages/product-categories-page/product-categories-page.ts
+++ happykopiApp/src/app/modules/products/product-pages/product-categories-page/product-categories-page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryListCard } from "../../components/category-list-card/category-list-card";
 import { CategoryWithProductCountDto } from '../../../../core/dtos/order/category-with-product-count.dto';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 export interface CategoryWithProductCountWithIdDto extends CategoryWithProductCountDto {
   id: number;
@@ -9,16 +10,22 @@ export interface CategoryWithProductCountWithIdDto extends CategoryWithProductCo
 
 @Component({
   selector: 'app-product-categories-page',
-  imports: [CategoryListCard],
+  imports: [CategoryListCard, CommonModule],
   templateUrl: './product-categories-page.html',
   styleUrl: './product-categories-page.css',
 })
 export class ProductCategoriesPage implements OnInit {
   categories !: CategoryWithProductCountDto[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.categories = this.route.snapshot.data['categorylist'];
+  }
+
+  onCategoryClick(categoryId: number) {
+    this.router.navigate(['/app/products'], {
+      queryParams: { categoryId: categoryId },
+    });
   }
 }
