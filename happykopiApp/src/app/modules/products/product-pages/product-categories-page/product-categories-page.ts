ec2f30fe529fd9e707751533af4f1b8f0f0e9b03
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-14 02:06:30 +0800

    Products Category and Search Bar Working

--- happykopiApp/src/app/modules/products/product-pages/product-categories-page/product-categories-page.ts
+++ happykopiApp/src/app/modules/products/product-pages/product-categories-page/product-categories-page.ts
@@ -16,11 +16,13 @@ export interface CategoryWithProductCountWithIdDto extends CategoryWithProductCo
 })
 export class ProductCategoriesPage implements OnInit {
   categories !: CategoryWithProductCountDto[];
+  totalDrinksCount = 0;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.categories = this.route.snapshot.data['categorylist'];
+    this.totalDrinksCount = this.categories.reduce((sum, category) => sum + category.productCount, 0);
   }
 
   onCategoryClick(categoryId: number) {
