c4f881a52e797cb82793ba0e997a89a0c846afcc
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-16 23:52:27 +0800

    Added migrations

---
+++ happykopiAPI/happykopiAPI/Data/Migrations/20251116154746_AddedOrderStoredProcedures.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace happykopiAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOrderStoredProcedures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp_GetAllAvailableAndActiveProducts = @"
+            CREATE OR ALTER PROCEDURE sp_GetAllAvailableAndActiveProducts
+            @CategoryId INT = NULL
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+
+                SELECT 
+                    p.Id,
+                    p.Name,
+                    p.ImageUrl,
+                    p.IsAvailable,
+                    p.CategoryId,
+                    c.Name AS CategoryName
+                FROM Products p
+                JOIN Categories c ON c.Id = p.CategoryId
+                WHERE 
+                p.IsActive = 1
+                AND (@CategoryId IS NULL OR p.CategoryId = @CategoryId);
+            END";
+
+            migrationBuilder.Sql(sp_GetAllAvailableAndActiveProducts);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS sp_GetAllAvailableAndActiveProducts");
+        }
+    }
+}
