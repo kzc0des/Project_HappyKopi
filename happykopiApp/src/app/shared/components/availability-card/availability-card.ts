80393ce6186e6f7eaae9ff040a8d18d83651c1d0
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-16 15:54:39 +0800

    Availability component refactored

--- happykopiApp/src/app/shared/components/availability-card/availability-card.ts
+++ happykopiApp/src/app/shared/components/availability-card/availability-card.ts
@@ -1,12 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-availability-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './availability-card.html',
   styleUrl: './availability-card.css',
 })
 export class AvailabilityCard {
 @Input() isAvailable: boolean = true;
 @Input() title: string = 'Available?';
+
+availabilityLabel(): string {
+  return this.isAvailable ? 'Available' : 'Not Available';
+}
 }
